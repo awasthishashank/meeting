39905d2f1f4dc65c4b016247de67b6119a8c5405
awasthishashank <awasthishashank@gmail.comgit config --global user.username awasthishashank git config --global user.email awasthishashank@gmail.com>
2024-06-24 19:15:53 +0530

    add meet details

---
+++ src/app/[meetupid]/page.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail"
+
+function MeetupDetails() {
+    return <>
+        <MeetupDetail
+            image="https://addicted2success.com/wp-content/uploads/2018/06/8-Reasons-You-Should-Join-a-Meetup-Group-Today.jpg"
+            title="A first meetup"
+            address="154,AB Nagar"
+            description="A first meet dicription"
+        />
+        </>
+
+}
+export default MeetupDetails
---
+++ src/app/new-meetup/page.js
@@ -0,0 +1,14 @@
+"use client"
+import NewMeetupForm from '@/components/meetups/NewMeetupForm'
+
+const  NewMeetupPage = () => {
+    function addMeetupHandler(enteredMeetupData){
+        console.log(enteredMeetupData)
+
+    }
+  return (<NewMeetupForm onAddMeetup = {addMeetupHandler}/>
+    
+  )
+}
+
+export default NewMeetupPage
